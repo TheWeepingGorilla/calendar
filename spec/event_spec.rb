3ac0d373068247482adb1cd96df3d0f23ef87346
Sean Morrison <student@epicodus.com>
2014-08-15 16:41:01 -0700

    Add validate, scope, and callback.

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe 'Event' do
-
+  it "returns events without a start time when called with that scope" do
+    new_event = Event.create({:description => "Hi", :location => "Ho"})
+    expect(Event.no_start[0]).to eq new_event
+  end
 end
